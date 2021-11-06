11d6e7f6f7c58ec1b810afef2ab3aca85298528b
Hauke Marquard <hauke.marquard@gmail.com>
2021-11-06 08:29:39 +0100

    Update delete kmean mnist

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from k_means_mnist import mnist_kmean
+
 from blob_clustering import blob_clustering
 import hierarchical_clustering
 import hierarchical_clustering_cluster2
@@ -9,11 +9,11 @@ _EXAMPLE_CENTERS = 2
 
 if __name__ == '__main__':
 
-    mnist_kmean()
+
 
     #blob_clustering()
 
-    #hierarchical_clustering()
+    hierarchical_clustering()
 
     #hierarchical_clustering_cluster2()
 
